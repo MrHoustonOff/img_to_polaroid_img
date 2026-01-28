e95fb17772b815acc91d8604805c709e349bd6ce
MrHouston <mrhoustonvr@yandex.ru>
2026-01-28 08:42:07 +0300

    Add validation constants (aspect ratio, min res).

---
+++ config.py
@@ -0,0 +1,19 @@
+
+
+# GEOMETRY
+
+# Боковые поля рамки
+BORDER_SIDE_RATIO = .06
+
+# Верхнее поле рамки
+BORDER_TOP_RATIO = .06
+
+# Нижнее поле рамки
+BORDER_BOTTOM_RATIO = .18
+
+# VALIDATION
+
+MIN_ASPECT_RATIO = .3
+MAX_ASPECT_RATIO = 3.0
+
+MIN_IMAGE_SIZE = 50
